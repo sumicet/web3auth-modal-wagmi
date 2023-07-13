51900febec5568633992686a178d73bac537ccff
sumicet <mihailescuc@outlook.com>
2023-07-13 21:20:31 +0300

    Handle case where the web3auth session expires

--- src/web3/connector.ts
+++ src/web3/connector.ts
@@ -4,24 +4,55 @@ import { Chain, ConnectorData, UserRejectedRequestError } from 'wagmi';
 import { LoginModal, LOGIN_MODAL_EVENTS } from '@web3auth/ui';
 
 class Connector extends WagmiWeb3AuthConnector {
+    private closeModal() {
+        (
+            this.web3AuthInstance as IWeb3AuthModal & { loginModal?: LoginModal }
+        )?.loginModal?.closeModal();
+    }
+
     async connect() {
         let error: any;
 
         const value = await new Promise(async (resolve, reject) => {
-            this.web3AuthInstance.on(LOGIN_MODAL_EVENTS.MODAL_VISIBILITY, (isVisible: boolean) => {
-                if (!isVisible && !this.provider) {
-                    // User closed the modal without connecting
-                    // Throw an error to notify `wagmi`
-                    // If you don't throw an error here, `wagmi` will think
-                    // that the user is still trying to connect and will remain in a
-                    // forever loading state
-                    reject(new UserRejectedRequestError('User rejected the request'));
+            this.web3AuthInstance.on(
+                LOGIN_MODAL_EVENTS.MODAL_VISIBILITY,
+                async (isVisible: boolean) => {
+                    /**
+                     * If the web3auth session is expired => the modal opens automatically.
+                     *
+                     * If the web3auth session is expired and the user calls `connect`
+                     * (usually during eager connection),
+                     * web3auth will open the modal => every time we refresh the page,
+                     * this modal will be opened => not the desired behavior.
+                     *
+                     * To prevent this, let's kill the wagmi connection.
+                     */
+                    const ls = localStorage.getItem('wagmi.connected');
+                    const isWagmiConnected = ls ? JSON.parse(ls) : ls;
+                    if (isVisible && isWagmiConnected) {
+                        this.closeModal();
+                        await this.disconnect();
+                        reject(new UserRejectedRequestError('User rejected the request'));
+                    }
+
+                    /**
+                     * The user closed the modal without connecting => throw an error to
+                     * notify `wagmi`. If you don't throw an error here, `wagmi` will think
+                     * that the user is still trying to connect and will remain in a
+                     * forever loading state.
+                     */
+                    if (!isVisible && !this.provider) {
+                        reject(new UserRejectedRequestError('User rejected the request'));
+                    }
                 }
-            });
+            );
 
             try {
                 const result = await super.connect();
-                // Make additional requests here if needed
+                // Make additional requests here if needed eg authenticate with a backend
+
+                this.closeModal();
+
                 resolve(result);
             } catch (error: any) {
                 reject(error);
@@ -36,16 +67,13 @@ class Connector extends WagmiWeb3AuthConnector {
             // Show a generic error message for the user
             console.error(error);
 
-            const web3AuthInstanceModal = this.web3AuthInstance as IWeb3AuthModal & {
-                loginModal?: LoginModal;
-            };
-
-            // Close the modal on error to prevent the user from attempting
-            // to connect to another provider
-            // If the user connects to another provider after an error,
-            // `wagmi` will not be able to detect that the user is connected
-            // since we're not calling `connect` again
-            web3AuthInstanceModal.loginModal?.closeModal();
+            /**
+             * Close the modal on error to prevent the user from attempting
+             * to connect to another provider. If the user connects to another
+             * provider after an error, `wagmi` will not be able to detect that
+             * the user is connected since we're not calling `connect` again.
+             */
+            this.closeModal();
         }
 
         throw error;
